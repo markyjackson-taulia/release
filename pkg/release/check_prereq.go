d18b1c4a7c1091a9ce80be2c77acb243d5674ba1
markyjackson-taulia <marky.r.jackson@gmail.com>
2020-11-16 09:15:04 -0800

    changing package name
    Signed-off-by: markyjackson-taulia <marky.r.jackson@gmail.com>

--- pkg/release/check_prereq.go
+++ pkg/release/check_prereq.go
@@ -1,12 +1,14 @@
-package release
+package CheckPrerequisites
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/blang/semver"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"k8s.io/release/pkg/git"
+	"k8s.io/release/pkg/release"
 	"k8s.io/release/pkg/util"
+	"k8s.io/release/pkg/version"
 )
+
